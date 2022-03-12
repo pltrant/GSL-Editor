fc9c7b87db1b32095cb0ee44e7096062da1512f8
Oliver Ark Kurek <anshou@gmail.com>
2022-03-12 00:47:43 -0500

    replace smart quotes with dumb variety filter more non printables

--- gsl/formattingEditProvider.ts
+++ gsl/formattingEditProvider.ts
@@ -11,9 +11,12 @@ export class GSLDocumentFormattingEditProvider implements DocumentFormattingEdit
         document.lineCount - 1,
         lastLine.range.end.character
       )
+      // Replace smart quotes and other common mistake with dumb variety
+      textEdits.push(TextEdit.replace(textRange, document.getText().replace(/[\u2018\u2019]/g, "'")))
+      textEdits.push(TextEdit.replace(textRange, document.getText().replace(/[\u201C\u201D]/g, '"')))
       // Remove non-printable characters
       // eslint-disable-next-line no-control-regex
-      textEdits.push(TextEdit.replace(textRange, document.getText().replace(/[^\x00-\x7f]/g, '')))
+      textEdits.push(TextEdit.replace(textRange, document.getText().replace(/[^\x07\x20-\x7e]/g, '')))
       // Remove blank lines
       textEdits.push(TextEdit.replace(textRange, document.getText().replace(/(\r\n){2,}/g, '\r\n')))
       return textEdits
