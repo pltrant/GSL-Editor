1effa66eea5cfa38f6d3fbe1f59ca494ea4bf8c5
Oliver Ark Kurek <anshou@gmail.com>
2022-03-14 10:57:26 -0400

    actually let's leave those low ASCII characters in place because they have new lines and stuff like that ugh

--- gsl/formattingEditProvider.ts
+++ gsl/formattingEditProvider.ts
@@ -16,7 +16,7 @@ export class GSLDocumentFormattingEditProvider implements DocumentFormattingEdit
       textEdits.push(TextEdit.replace(textRange, document.getText().replace(/[\u201C\u201D]/g, '"')))
       // Remove non-printable characters
       // eslint-disable-next-line no-control-regex
-      textEdits.push(TextEdit.replace(textRange, document.getText().replace(/[^\x07\x20-\x7e]/g, '')))
+      textEdits.push(TextEdit.replace(textRange, document.getText().replace(/[^\x00\x20-\x7e]/g, '')))
       // Remove blank lines
       textEdits.push(TextEdit.replace(textRange, document.getText().replace(/(\r\n){2,}/g, '\r\n')))
       return textEdits
